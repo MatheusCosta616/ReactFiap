a6b9aa25386a0b5c8b34c3162cd86e30a23b7115
MatheusCosta616 <rm551157@fiap.com>
2023-08-22 11:56:41 -0300

    upa

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Cabecalho from './cabecalho/cabecalho.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Cabecalho></Cabecalho>
     <App />
   </React.StrictMode>,
 )
